1aa7aad1861c4240eb9229b57ebef4e158352c79
Ole Jørgensen <olejorgensen@me.com>
2024-05-12 17:15:46 +0200

    Update TextDataFormatProvider.cs

--- DragAndDrop/TextDataFormatProvider.cs
+++ DragAndDrop/TextDataFormatProvider.cs
@@ -11,6 +11,23 @@ namespace WpfPlayground
             TextDataFormat.Text
         };
 
+        public static (IHtmlDocumentContent? Contents, Exception? Error) GetHtmlDocumentContent(DataObject? dataObject)
+        {
+            (IHtmlDocumentContent? Contents, Exception? Error) result = (null, null);
+            try
+            {
+                if (dataObject?.GetText(TextDataFormat.Html) is string htmlString && !string.IsNullOrWhiteSpace(htmlString))
+                {
+                    result.Contents = HtmlTextProvider.TryGetHtmlDocumentContent(htmlString);
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Error = ex;
+            }
+            return result;
+        }
+
         public static string? GetText(DataObject? obj)
         {
             if (obj is not null)
@@ -22,6 +39,34 @@ namespace WpfPlayground
                     }
                 }
             return null;
+        }
+    }
+
+    public static class HtmlTextProvider
+    {
+        public static Uri? GetBaseUri(string htmlData)
+        {
+            var baseUri = new Uri(Regex.Match(htmlData, @"SourceURL:(.+)\n").Groups[1].Value.Trim());
+            return baseUri;
+        }
+
+        public static IHtmlDocumentContent? TryGetHtmlDocumentContent(string htmlData)
+        {
+            var startHtmlIndex = int.Parse(Regex.Match(htmlData, @"StartHTML:(\d+)").Groups[1].Value);
+            //var endHtmlIndex = int.Parse(Regex.Match(htmlData, @"EndHTML:(\d+)").Groups[1].Value);
+            var baseUri = new Uri(Regex.Match(htmlData, @"SourceURL:(.+)\n").Groups[1].Value.Trim());
+            var html = htmlData.Substring(startHtmlIndex);
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var service = new HtmlDocumentContentService
+            {
+                Document = doc,
+                BaseUri = baseUri
+            };
+
+            return service.GetContents();
         }
     }
 }
